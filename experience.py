0f5f934a9c9300912db03f4b613ce0f854211234
Lucas Pluvinage <lucas.pluvinage@ens.fr>
2017-08-10 16:14:38 +0200

    updated for a new series of experiments

--- experience.py
+++ experience.py
@@ -1,18 +1,17 @@
 from __future__ import print_function
 import os
-for nmodes in [2]:
+for nmodes in [0]:
     print(' nmodes:',nmodes)
-    for boardSz in [2,3]:
+    for boardSz in [2]:
         print('  boardSz:',boardSz)
-        for kind in ['one','mul']:
+        for kind in ['mul']:
             print('   kind:', kind)
-            for annealing in [True]:
-                print('    annealing:',annealing)
-                name = 'experiment2/{}_{}_{}_{}.pkl'.format(nmodes,boardSz,kind,annealing)
-                if not(os.path.isfile(name)):
-                    nsq = boardSz**2
-                    input_dataset = 'dataset_{}x{}_{}.pkl'.format(nsq,nsq,kind)
-                    command = 'python sudoku_learn.py --boardSz {} --lognmodes {} --dataset {} --out {}'.format(boardSz,nmodes,input_dataset,name)
-                    if annealing:
-                        command = command + ' --annealing'
-                    os.system(command)
+            for decay in [4,8,16,32]:
+                print('    decay:',decay)
+                for i in range(10):
+                    name = 'experiment10/{}_{}_{}_{}_{}.pkl'.format(nmodes,boardSz,kind,decay,i)
+                    if not(os.path.isfile(name)):
+                        nsq = boardSz**2
+                        input_dataset = 'dataset_{}x{}_{}.pkl'.format(nsq,nsq,kind)
+                        command = 'python sudoku_learn.py --boardSz {} --nopad --k 5 --h 10 --bs 200 --nepoch 50 --decay {} --lognmodes {} --dataset {} --out {}'.format(boardSz,decay,nmodes,input_dataset,name)
+                        os.system(command)
