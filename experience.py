e6152127fa766ab467998bd4a2fd26ebad96efc8
Lucas Pluvinage <lucas.pluvinage@ens.fr>
2017-08-14 11:53:15 +0200

    full experiments+judge

--- experience.py
+++ experience.py
@@ -1,17 +1,79 @@
 from __future__ import print_function
 import os
-for nmodes in [0]:
-    print(' nmodes:',nmodes)
-    for boardSz in [2]:
-        print('  boardSz:',boardSz)
-        for kind in ['mul']:
-            print('   kind:', kind)
-            for decay in [4,8,16,32]:
-                print('    decay:',decay)
-                for i in range(10):
-                    name = 'experiment10/{}_{}_{}_{}_{}.pkl'.format(nmodes,boardSz,kind,decay,i)
-                    if not(os.path.isfile(name)):
-                        nsq = boardSz**2
-                        input_dataset = 'dataset_{}x{}_{}.pkl'.format(nsq,nsq,kind)
-                        command = 'python sudoku_learn.py --boardSz {} --nopad --k 5 --h 10 --bs 200 --nepoch 50 --decay {} --lognmodes {} --dataset {} --out {}'.format(boardSz,decay,nmodes,input_dataset,name)
-                        os.system(command)
+import judge
+
+
+experiences = [# boardSz,nprelearn,nlearn,lognmodes,k,h,annealing,nopadding,dataset_difficulty
+                # the difficulty of simple learning on difficult dataset, even with padding
+                (2,0,50,0,1,0,False,False,"one"),
+                (2,0,50,0,1,0,False,False,"mul"), 
+                (2,0,50,0,1,0,False,False,"mul_hard"),
+                # multi-modal mean-field overcome this problem:
+                (2,0,50,2,1,0,False,False,"one"),
+                (2,0,50,2,1,0,False,False,"mul"),
+                (2,0,50,4,1,0,False,False,"mul_hard"),
+                # but removing the padding fucks everything:
+                (2,0,50,0,1,0,False,True,"one"),
+                (2,0,50,2,1,0,False,True,"mul"),
+                # so let's add multiple filters possibility:
+                (2,0,50,0,10,20,False,True,"one"),
+                # and with multi-modal on difficult datasets:
+                (2,0,50,2,10,20,False,True,"mul"),
+                # with pre-learning to get even better results:
+                (2,40,10,2,10,20,False,True,"mul"),
+                # On 9x9:
+                (3,40,10,2,10,20,False,True,"mul"),
+                (3,0,50,0,10,20,False,True,"mul"),
+                (3,0,50,2,10,20,False,True,"mul"),
+                (3,40,10,0,10,20,False,True,"one"),
+                (2,40,10,2,10,20,True,True,"mul"),
+                (3,40,25,2,10,20,False,True,"mul"),
+                (3,0,65,2,10,20,False,True,"mul")
+
+            ]
+
+
+
+for i,(boardSz, nprelearn, nepoch, lognmodes, k, h, annealing, nopad, dataset) in enumerate(experiences):
+    name = 'fullexp/' + ("_".join([str(x) for x in list(experiences[i])]))
+    if not(os.path.isfile(name)):
+        print(name)
+        nsq = boardSz**2
+        input_dataset = 'dataset/dataset_{}x{}_{}.pkl'.format(nsq,nsq,dataset)
+        command = 'python sudoku_learn.py --bs 200 --decay 16'
+        command += ' --boardSz {}'.format(boardSz)
+        command += ' --nprelearn {}'.format(nprelearn)
+        command += ' --nepoch {}'.format(nepoch)
+        command += ' --lognmodes {}'.format(lognmodes)
+        command += ' --k {}'.format(k)
+        command += ' --h {}'.format(h)
+        command += ' --dataset {}'.format(input_dataset)
+        command += ' --out {}'.format(name)
+        if annealing:
+            command += ' --annealing'
+        if nopad:
+            command += ' --nopad'
+        print(command)
+        os.system(command)
+
+
+
+res_exp = open('results.txt','w')
+for i,(boardSz, nprelearn, nepoch, lognmodes, k, h, annealing, nopad, dataset) in enumerate(experiences):
+    name = 'fullexp/' + ("_".join([str(x) for x in list(experiences[i])]))
+    nsq = boardSz ** 2
+    dataset = 'dataset/dataset_{}x{}_mul_2.pkl'.format(nsq,nsq)
+    for lognmodes in [0,2]:
+        args = {'dataset': dataset,
+                'input': name,
+                'boardSz': boardSz,
+                'bs': 200, 
+                'lognmodes': lognmodes
+                } 
+        n_correct, n_samples, n_correct_tot, n_tot = judge.judge(args)
+        p_correct = 100* (n_correct / float(n_samples))
+        p_explored = 100* (n_correct_tot / float(n_tot))
+        res_exp.write('{} : {} | {}\n'.format(name, p_correct, p_explored))
+        
+
+close(res_exp)
